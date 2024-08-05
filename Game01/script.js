c74fdea444b4c0d1d2fcee453da54567d9dc170d
Monicah Koina <monicahkoina1@gmail.com>
2024-08-05 23:26:50 +0300

    HTML5 canvas set up

---
+++ Game01/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const cxt = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+    
+})
