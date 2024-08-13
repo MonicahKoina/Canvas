d13f6a8f99fa163a5387f3725e2c775b4a72973d
Monicah Koina <monicahkoina1@gmail.com>
2024-08-13 12:31:50 +0300

    game and player class

--- Game01/player.js
+++ Game01/player.js
@@ -1,5 +1,17 @@
 class player{
     constructor(game){
         this.game=game;
+        this.width=100;
+        this.height=91.3;
+        this.x=0;
+        this.y=100;
+    }
+    //move it around based on the user's input
+    update(){
+
+    }
+    //draw current frame and co-ordinates
+    draw(context){
+        context.fillRect(this.x, this.y, this.width, this.height)
     }
 }
